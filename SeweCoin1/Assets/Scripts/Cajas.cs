330b6dcd4abf13231044235e2848ead8ef79a025
Usuario Local <Usuario Local>
2018-04-13 11:22:54 +0200

    Añadido Cajas.cs

---
+++ SeweCoin1/Assets/Scripts/Cajas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cajas : MonoBehaviour {
+
+	public GameObject objeto;
+	GameObject objetosuelto;
+
+	public void Spawn () {
+		objetosuelto = Instantiate (objeto);
+		objetosuelto.transform.position = new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y + 1);
+	}
+
+
+}
