ac605ed886a1eb4619c4bccb9eda4c28f5b6261a
Gonzalo Cidoncha <gcidonch@ucm.es>
2018-04-13 18:02:39 +0200

    Primer intento de añadir Knockback, modificado DanioEnemigos

--- SeweCoin1/Assets/Scripts/DanioEnemigos.cs
+++ SeweCoin1/Assets/Scripts/DanioEnemigos.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class DanioEnemigos : MonoBehaviour {
 
-	public int cantDanio;
+	public int cantDanio, knockbackx, knockbacky;
+	Rigidbody2D rb;
+	public GameObject player;
+
+	void Awake () {
+		rb = player.GetComponent<Rigidbody2D> ();
+	}
+
 	void OnCollisionEnter2D(Collision2D col){
-		if (col.gameObject.tag == "Player")
+		if (col.gameObject.tag == "Player") {
 			col.gameObject.GetComponent<PlayerController> ().QuitaVida (cantDanio);
+
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D col){
-		if (col.gameObject.tag == "Player")
+		if (col.gameObject.tag == "Player") {
+			if (gameObject.transform.position.x > col.gameObject.transform.position.x)
+				rb.AddForce (new Vector2 (-knockbackx, knockbacky), ForceMode2D.Impulse);
+			else
+				rb.AddForce (new Vector2 (knockbackx, knockbacky), ForceMode2D.Impulse);
 			col.gameObject.GetComponent<PlayerController> ().QuitaVida (cantDanio);
+		}
 	}
 
 	void OnTriggerStay2D(Collider2D col){
