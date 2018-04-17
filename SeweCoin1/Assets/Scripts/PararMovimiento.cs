eb8df9523a689d13d6a3d606e961a64dc43a1bf3
Posna <37537187+Posna@users.noreply.github.com>
2018-04-17 17:44:40 +0200

    Boss1 terminado a espera de cambiar velocidades etc

---
+++ SeweCoin1/Assets/Scripts/PararMovimiento.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PararMovimiento : MonoBehaviour {
+
+	public float tiempoDeEspera;
+	public Camera camara;
+
+	void OnTriggerEnter2D(Collider2D col){
+		col.gameObject.GetComponent<PlayerController> ().CancelaMov (tiempoDeEspera);
+		camara.GetComponent<CameraController> ().enabled = false;
+		camara.transform.position = new Vector3 (11f, -1.5f, -10f);
+		camara.GetComponent<Camera> ().fieldOfView = 86.18258f; 
+		Destroy (gameObject);
+	}
+}
---
+++ SeweCoin1/Assets/Scripts/TriggerMov.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMov : MonoBehaviour {
+
+	public GameObject jhonny;
+	public int mover;
+
+	void OnTriggerEnter2D(Collider2D col){
+		jhonny.GetComponent<Jhonny> ().Mover (mover);
+		Destroy (gameObject);
+	}
+}
