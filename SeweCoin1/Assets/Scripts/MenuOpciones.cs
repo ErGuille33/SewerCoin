26ce02203d53644100047adfd21b1c9ee42e6ebe
Alberto Reymond González <areymond@ucm.es>
2018-04-22 20:36:05 +0200

    Menu 0.2
    Añadido el boton de pantalla completa, las resoluciones y la calidad gráfica.

--- SeweCoin1/Assets/Scripts/MenuOpciones.cs
+++ SeweCoin1/Assets/Scripts/MenuOpciones.cs
@@ -2,12 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuOpciones : MonoBehaviour {
 
 	public AudioMixer audioMixer;
+	public Dropdown ListaResoluciones;
+
+	Resolution[] resoluciones;
+
+	void Start(){
+		resoluciones = Screen.resolutions;
+
+		ListaResoluciones.ClearOptions ();
+
+		List<string> options = new List<string> ();
+
+		int currentResolutionIndex = 0;
+		for (int i = 0;	i < resoluciones.Length; i++) {
+
+			string option = resoluciones[i].width + "x" + resoluciones[i].height;
+			options.Add (option);
+
+			if (resoluciones [i].width == Screen.currentResolution.width && resoluciones [i].height == Screen.currentResolution.height)
+				currentResolutionIndex = i;
+		}
+
+		ListaResoluciones.AddOptions (options);
+		ListaResoluciones.value = currentResolutionIndex;
+		ListaResoluciones.RefreshShownValue ();
+
+	}
+
+	public void Resolucion(int resolutionIndex){
+
+		Resolution resolucion = resoluciones [resolutionIndex];
+		Screen.SetResolution (resolucion.width, resolucion.height, Screen.fullScreen);
+	}
 
 	public void Volumen (float volumen){
+		
 		audioMixer.SetFloat ("VolumenPrincipal", volumen);
+	}
+
+	public void Graficos(int qualityIndex){
+		
+		QualitySettings.SetQualityLevel (qualityIndex);
+	}
+
+	public void PantallaCompleta (bool esCompleta){
+		
+		Screen.fullScreen = esCompleta;
 	}
 }
