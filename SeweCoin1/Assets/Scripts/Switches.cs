cb59321a0649701ac81eb44067baed78a966a159
q <q>
2018-05-04 12:58:49 +0200

    Animacion Palanca y Camara Valvula

--- SeweCoin1/Assets/Scripts/Switches.cs
+++ SeweCoin1/Assets/Scripts/Switches.cs
@@ -6,18 +6,43 @@ public class Switches : MonoBehaviour {
 
 	public Collider2D ataque;
 	public float tiempo;
-
+	public GameObject camara;
+	Animator anim;
 	public GameObject puerta;
 
+	void Start()
+	{
+		anim = GetComponentInParent<Animator> ();
+		if(gameObject.tag!="Valvula")
+		anim.SetBool ("Activo", false);
+	}
 	void OnTriggerEnter2D (Collider2D col) {
 		print ("hi");
 		if (col == ataque || col.gameObject.tag == "bala") {
-			print ("ho");
+			
+			if (gameObject.tag == "Valvula")
+			{
+				camara.GetComponent<CameraController> ().enabled = false;
+				Invoke ("CambiarCamara", 0.5f);
+
+			}
+			else anim.SetBool ("Activo", true);
 			Invoke ("Destroy", tiempo);
+
 		}
 	}
 
 	void Destroy(){
 		Destroy (puerta);
+		Invoke ("DevolverControlCamara", 1);
 	}
+	void DevolverControlCamara()
+	{
+		camara.GetComponent<CameraController> ().enabled = true;	
+	}
+	void CambiarCamara()
+	{
+		camara.transform.position =  new Vector3(24.1f,-59.7f,-10f);
+	}
+
 }
