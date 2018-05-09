38d71a9e2b144857a935232b4f3bb2b28827d904
Areymond <Areymond>
2018-05-09 10:14:07 +0200

    Arreglado el tamaño del Raycast y collider Alarico
    Arreglado el collider de Alarico para que no se quede trabado cuando la
    cabeza choca con algo y el tamaño del Raycast de salto

--- SeweCoin1/Assets/Scripts/RayCast.cs
+++ SeweCoin1/Assets/Scripts/RayCast.cs
@@ -14,16 +14,17 @@ public class RayCast : MonoBehaviour {
 
 
 	public bool DetectaMuro(float move){
-
-		RaycastHit2D hit = Physics2D.BoxCast(gameObject.transform.position, new Vector2 (1.66f/2f, 2.5f), angleMuro, new Vector2 (move, 0), distanciaMuro, capas);
+        //Crea una un Raycast hacia el lado al que esta mirando alarico para comprobar si esta pegado a una pared
+        RaycastHit2D hit = Physics2D.BoxCast(gameObject.transform.position, new Vector2 (1.66f/2f, 2.5f), angleMuro, new Vector2 (move, 0), distanciaMuro, capas);
 		if (hit.collider != null)
 			return true;
 		else return false;
 		}
 
 	public bool DetectaPlataforma(){
-
-		RaycastHit2D hit = Physics2D.BoxCast(gameObject.transform.position, new Vector2 (1.66f/2f, 1f), anglePlataforma, new Vector2 (0, -1), distanciaPlataforma, capas);
+    
+        //Crea una un Raycast debajo de alarico con forma de caja para detectar si puede saltar
+		RaycastHit2D hit = Physics2D.BoxCast(gameObject.transform.position, new Vector2 (0.5f/2f, 1f), anglePlataforma, new Vector2 (0, -1), distanciaPlataforma, capas);
 		if (hit.collider != null)
 			return true;
 		else return false;
