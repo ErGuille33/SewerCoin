bf652e77fe2c275dc8d295af625f59e7d66b9deb
Guiller <a>
2018-05-16 10:44:40 +0200

    Mejoras a los tiempos
    Todavia no esta al 100%

--- SeweCoin1/Assets/Scripts/GameManager.cs
+++ SeweCoin1/Assets/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour {
 	float timer, timeraux;
 	public int enemigosmatados;
 	StreamWriter tiempos;
+	StreamReader timepos;
 
 
 	//Awake is always called before any Start functions
@@ -40,6 +41,15 @@ public class GameManager : MonoBehaviour {
 
 		timeractivado = true;
 		enemigosmatados = 0;
+
+
+		tiempos = new StreamWriter (Application.dataPath + "/ultimotiempo.txt");
+		tiempos.WriteLine ("0");
+		tiempos.Close ();
+
+		tiempos = new StreamWriter (Application.dataPath + "/mejortiempo.txt");
+		tiempos.WriteLine ("10000000");
+		tiempos.Close ();
 
 		timer = 0f;
 	}
@@ -88,10 +98,23 @@ public class GameManager : MonoBehaviour {
 		timer = timeraux;
 	}
 
+	public string LeeMejorTiempo(){
+		string devuelve = "";
+		timepos = new StreamReader (Application.dataPath + "/mejortiempo.txt");
+		devuelve += timepos.ReadLine ();
+		timepos.Close ();
+		return devuelve;
+	}
+
 	public void GuardaTiempos () {
-		tiempos = new StreamWriter (Application.persistentDataPath + "/tiempos.txt");
-		timeraux = timer;
+		GuardaTimer ();
+		tiempos = new StreamWriter (Application.dataPath + "/ultimotiempo.txt");
 		tiempos.WriteLine (timeraux);
 		tiempos.Close ();
+		if (timeraux < int.Parse(LeeMejorTiempo())) {
+			tiempos = new StreamWriter (Application.dataPath + "/mejortiempo.txt");
+			tiempos.WriteLine (timeraux);
+			tiempos.Close ();
+		}
 	}
 }
