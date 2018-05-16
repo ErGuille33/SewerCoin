12ad0d5d3578676ba95421d92c5636e289f604b7
Alberto Reymond González <areymond@ucm.es>
2018-05-16 10:38:06 +0200

    Versión Pruebas

--- SeweCoin1/Assets/Scripts/RayCast.cs
+++ SeweCoin1/Assets/Scripts/RayCast.cs
@@ -24,7 +24,7 @@ public class RayCast : MonoBehaviour {
 	public bool DetectaPlataforma(){
     
         //Crea una un Raycast debajo de alarico con forma de caja para detectar si puede saltar
-		RaycastHit2D hit = Physics2D.BoxCast(gameObject.transform.position, new Vector2 (0.5f/2f, 1f), anglePlataforma, new Vector2 (0, -1), distanciaPlataforma, capas);
+		RaycastHit2D hit = Physics2D.BoxCast(gameObject.transform.position, new Vector2 (1.20f/2f, 1f), anglePlataforma, new Vector2 (0, -1), distanciaPlataforma, capas);
 		if (hit.collider != null)
 			return true;
 		else return false;
